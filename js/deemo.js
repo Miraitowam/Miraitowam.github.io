8ac09963984cf6c2a82e3662a8100ab82d6ceba4
navigator <mgx18742515453@outlook.com>
2020-10-23 22:31:40 +0800

    Site updated: 2020-10-23 22:31:33

--- js/deemo.js
+++ js/deemo.js
@@ -79,3 +79,25 @@ window.onload = function () {
         }
     }
 }
+// 调用一言
+window.onload = function () {
+    var hitokoto = document.querySelector('.hitokoto');
+    var from = document.querySelector('.from');
+    update();
+    function update() {
+        gethi = new XMLHttpRequest();
+        gethi.open("GET", "https://v1.hitokoto.cn/?c=j");
+        //这里选择类别，详见官方文档
+        gethi.send();
+        gethi.onreadystatechange = function () {
+            if (gethi.readyState === 4 && gethi.status === 200) {
+                var Hi = JSON.parse(gethi.responseText);
+                hitokoto.innerHTML = Hi.hitokoto;
+                if (Hi.from_who != null)
+                    from.innerHTML = Hi.from_who + " · " + "《 " + Hi.from + " 》"; //可自定义输出格式
+                else
+                    from.innerHTML = " “ " + Hi.from + " ” "
+            }
+        }
+    }
+}
